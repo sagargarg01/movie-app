c5a74988234e97ba5a61705a6447142a98f1fb54
Sagar Garg <sagargupta1417@gmail.com>
2020-06-01 20:45:36 +0530

    added navbar and moviecard components

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Navbar extends React.Component {
+
+   render(){
+      return (
+         <div className="search-container">
+           <div className="nav">
+              <input />
+              <button id="search-btn">Search</button>
+           </div>
+         </div>
+       );
+   }
+}
+
+export default Navbar;
